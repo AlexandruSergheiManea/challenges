954a234cc2e1daa2388f7191bf9b56a591af65a2
Alex <alexandru_manea@hotmail.com>
2023-09-18 10:17:51 +0100

    first commit

---
+++ python challanges.py
@@ -0,0 +1,32 @@
+def challange1():
+    height = int(input("whats the height"))
+    length = int(input("wahts the length"))
+    trigArea = (height * length / 2)
+    print (trigArea)
+
+   
+def challange3(num, numm):
+    num = int(num)
+    numm = int(numm)
+
+ 
+
+    if num % 7 == 0 or numm % 7 == 0:
+        print(num / numm)
+    elif num % 11 == 0 or numm % 11 == 0:
+        print( num * numm)
+
+    print(num + numm)
+
+
+
+def challange4():
+    years = int(input("what years is it"))
+    days = years*365
+    print (days)
+
+
+challange3(7, 11)
+    
+        
+    
